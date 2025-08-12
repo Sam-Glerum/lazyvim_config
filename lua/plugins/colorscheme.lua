ec61253b3b88625ef37371cbc413b6d829825e08
Sam Glerum <glerum@bitbucket.org>
2025-08-12 15:34:38 +0200

    uploading config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,31 @@
+return {
+  -- -- add gruvbox
+  -- { "ellisonleao/gruvbox.nvim" },
+  --
+  -- -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
+  -- add gruvbox
+  -- { "sainnhe/everforest", opts = { transparent_background = true } },
+  --
+  -- -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "everforest",
+  --   },
+  -- },
+  { "scottmckendry/cyberdream.nvim", opts = { transparent = true, saturation = 0.7 } },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
+}
