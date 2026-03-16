ef23436adba9b5e6451ec0adb2c43a84d96883ab
Sam Glerum <samuelglerum@hotmail.com>
2026-03-16 08:28:18 +0100

    backup cachy config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,12 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    {import = "lazyvim.plugins.extras.formatting.prettier"},
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      lazy = false,
+      build = ":TSUpdate",
+    },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -52,3 +57,28 @@ require("lazy").setup({
     },
   },
 })
+
+local git_root = vim.fs.root(0, ".git")
+
+if git_root ~= nil then
+  vim.lsp.config("vimfony", {
+    cmd = { "vimfony" },
+    filetypes = { "php", "twig", "yaml", "xml" },
+    root_markers = { ".git" },
+    single_file_support = true,
+    init_options = {
+      roots = { "templates" },
+      container_xml_path = (git_root .. "/var/cache/dev/App_KernelDevDebugContainer.xml"),
+      -- OR:
+      -- container_xml_path = {
+      --   (git_root .. "/var/cache/dev/App_KernelDevDebugContainer.xml"),
+      --   (git_root .. "/var/cache/website/dev/App_KernelDevDebugContainer.xml"),
+      --   (git_root .. "/var/cache/admin/dev/App_KernelDevDebugContainer.xml"),
+      -- },
+      vendor_dir = git_root .. "/vendor",
+      -- Optional:
+      -- php_path = "/usr/bin/php",
+    },
+  })
+  vim.lsp.enable("vimfony")
+end
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,12 +3,13 @@ return {
   -- { "ellisonleao/gruvbox.nvim" },
   --
   -- -- Configure LazyVim to load gruvbox
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "gruvbox",
-  --   },
-  -- },
+  { "sainnhe/everforest" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
   -- add gruvbox
   -- { "sainnhe/everforest", opts = { transparent_background = true, priority = 1000 } },
 
@@ -31,13 +32,13 @@ return {
   --   },
   -- },
   {
-    "zenbones-theme/zenbones.nvim",
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = "rktjmp/lush.nvim",
-    lazy = false,
-    priority = 1000,
+    -- "zenbones-theme/zenbones.nvim",
+    -- -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    -- dependencies = "rktjmp/lush.nvim",
+    -- lazy = false,
+    -- priority = 1000,
     -- you can set set configuration options here
     -- config = function()
     --     vim.g.zenbones_darken_comments = 45
@@ -46,10 +47,10 @@ return {
   },
   --
   -- -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "zenbones",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "zenbones",
+  --   },
+  -- },
 }
